5dc40b2ebe5900519a2b495c37f5d0e87a548ebe
gustavo-dev <gvwchevrand@gmail.com>
2021-12-11 09:42:52 -0300

    build: :construction_worker: Set build script to remove unnecessary .js files when comipling

--- .task/build.mjs
+++ .task/build.mjs
@@ -3,3 +3,7 @@ import shelljs from "shelljs"
 shelljs.rm("-rf", "dist")
 
 shelljs.exec("tsc")
+
+// Removing .js files generated on typings
+shelljs.cd("dist/typings")
+shelljs.exec('find . -type f -name "*.js" -exec rm -rf {} +')
